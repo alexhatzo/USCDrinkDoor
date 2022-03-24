69618603a08b6d192ceaba2854c418f92dee24fa
cw130140 <yonghaow@usc.edu>
2022-03-24 16:44:05 -0700

    Connecting activities

--- app/src/main/java/com/example/uscdrinkdoor/Seller_Profile.java
+++ app/src/main/java/com/example/uscdrinkdoor/Seller_Profile.java
@@ -3,6 +3,8 @@ package com.example.uscdrinkdoor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.content.Intent;
 
 public class Seller_Profile extends AppCompatActivity {
 
@@ -10,9 +12,10 @@ public class Seller_Profile extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_seller_profile);
+    }
 
-
-
-
+    public void goback(View view) {
+        Intent intent = new Intent(this, MapsActivity.class);
+        startActivity(intent);
     }
 }
